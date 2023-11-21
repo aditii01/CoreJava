953fceb668bd82d9ec6d61428725a848a69a7472
aditii01 <89338034+aditii01@users.noreply.github.com>
2023-11-21 21:19:37 +0530

    Create CloneObject.java

---
+++ CloneObject.java
@@ -0,0 +1,15 @@
+
+
+
+public class CloneObject 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Clone Object");
+    }
+}
+
+//1. create branch
+//2. write prog and commit to branch
+//3. create pull request to main
+//4. assign me the reviewer for pull request (PR)
